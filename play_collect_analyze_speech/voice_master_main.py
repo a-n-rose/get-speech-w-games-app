944b77d36d51edb3d807fef2f8a3821bc4332ebe
Aislyn Rose <no email yet>
2018-05-17 13:30:12 +0200

    added start_game() func: cleaned up code a touch

--- play_collect_analyze_speech/voice_master_main.py
+++ play_collect_analyze_speech/voice_master_main.py
@@ -8,37 +8,40 @@ Created on Tue May 15 22:22:15 2018
 
 from voice_master import Mimic_Game
 
+
+def start_game():
+    user_ready = input("Press ENTER to start or type 'exit' to leave: ")
+    if user_ready == '':
+        user_name = input("Great! What will be your username?: ")
+        return user_name
+    elif 'exit' in user_ready.lower():
+        return None
+    else:
+        start_game()
+    
+    
+
 if __name__ == '__main__':
     print("Welcome to the Mimic Mastery Game!")
-    user_ready = input("Press ENTER to start")
-    user_start = False
-    while user_start == False:
-        if user_ready:
-            user_ready = input("Press ENTER to start. To exit, type 'exit': ")
-            if 'exit' in user_ready.lower():
-                break
+    username = start_game()
+    if username:
+        user_game = Mimic_Game(username)
+        test_start = input("Ready to test your mic? (press y or n): ")
+        test = False
+        while test == False:
+            if 'n' in test_start:
+                print("Why did you start the game then? Give it a try!")
+            if 'y' in test_start:
+                test = True
+        print("Recoring...")
+        user_rec = user_game.test_record()
+        print("Stopped recording and.....")
+        if user_rec.any():
+            print("It worked!")
+            print("And here's how you sounded:")
+            user_game.play_rec(user_rec)
         else:
-            user_start = True
-    
-    username = input("What's your name? ")
-    user_game = Mimic_Game(username)
-    
-    test_start = input("Ready to test your mic? (press y or n): ")
-    test = False
-    while test == False:
-        if 'n' in test_start:
-            print("Why did you start the game then? Give it a try!")
-        if 'y' in test_start:
-            test = True
-    print("Recoring...")
-    user_rec = user_game.test_record()
-    print("Stopped recording and.....")
-    if user_rec.any():
-        print("It worked!")
-        print("And here's how you sounded:")
-        user_game.play_rec(user_rec)
-    else:
-        print("Oh darnit... Something's wrong...")
+            print("Oh darnit... Something's wrong...")
+            
         
-    
-        
+            
