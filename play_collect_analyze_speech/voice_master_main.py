daf525176477dcc730cfd01c22d32463891bf3c5
Aislyn Rose <no email yet>
2018-05-18 11:29:37 +0200

    include option for user to hear more sounds or to exit

--- play_collect_analyze_speech/voice_master_main.py
+++ play_collect_analyze_speech/voice_master_main.py
@@ -11,14 +11,21 @@ from voice_master import Mimic_Game
 
 if __name__ == '__main__':
     
-    new_game = Mimic_Game()
-    username = new_game.start_game('start', username = True)
+    currgame = Mimic_Game()
+    username = currgame.start_game('start', username = True)
     if username:
         sec = 2
-        mictest = new_game.test_mic(sec)
+        mictest = currgame.test_mic(sec)
         if mictest == False:
             print("We couldn't test your voice..")
-        listen = new_game.start_game('listen to a sound')
-        if listen:
-            new_game.rand_sound2mimic()
+        while currgame.cont_game == True:
+            currgame.cont_game = currgame.start_game('listen to a sound')
+            if currgame.cont_game:
+                currgame.rand_sound2mimic()
+            else:
+                print("Thanks for playing!")
+                currgame.close_game()
+            
+        
+        
          
