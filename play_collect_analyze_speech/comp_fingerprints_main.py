fdd480f842d16f2ff4e0c8a313c95cd0d0d1de53
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-05-19 18:19:28 +0200

    imported librosa: made recording of user mimic same duration as the sound file they mimicked

--- play_collect_analyze_speech/comp_fingerprints_main.py
+++ play_collect_analyze_speech/comp_fingerprints_main.py
@@ -8,24 +8,33 @@ Created on Sat May 19 13:59:15 2018
 
 from comp_fingerprints import Comp_FP
 import datetime
+import librosa
 
 class Context:
     def __init__(self,comp_name):
         self.comp_name = comp_name
         self.date = None
 
+
+def get_duration(wavefile):
+    y, fs = librosa.load(wavefile)
+    print(fs)
+    duration = len(y)/fs
+    return(duration)
+
 if __name__ == '__main__':
     comp_name = input("Name this fingerprint comparison: ")
     currcont = Context(comp_name)
     file1 = 'dove-Mike_Koenig-1208819046_orig.wav'
-    file2 = 'usr_rec_doveVSgrrrr.wav'
+    file2 = None
     curr_fp = Comp_FP(file1,file2)
     
     if curr_fp.file2 == None:
         print("Try to mimic this sound:")
         curr_fp.play_wav(file1)
         print("Now recording!")
-        usr_wav = curr_fp.record_user(5)
+        duration = get_duration(file1)
+        usr_wav = curr_fp.record_user(duration)
         time = datetime.datetime.now()
         time_str = "{}".format(str(time.year)+'_'+str(time.day)+'_'+str(time.hour)+'_'+str(time.minute))
         currcont.date = time_str
