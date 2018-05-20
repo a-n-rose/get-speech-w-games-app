a4fadfda517f9d8d40c40c5459594d9ca4121eb4
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-05-20 12:14:12 +0200

    Included point tracker: once user reaches a certain amount (currently 1000) the game stops

--- play_collect_analyze_speech/voice_master_main.py
+++ play_collect_analyze_speech/voice_master_main.py
@@ -28,22 +28,29 @@ if __name__ == '__main__':
         if mictest == False:
             print("We couldn't test your voice..")
         while currgame.cont_game == True:
-            currgame.cont_game = currgame.start_game('listen to a sound')
-            if currgame.cont_game:
-                print("Right after this plays, we will record your attempt at the sound. Get ready!")
-                mim_filename = directory_mim+currgame.rand_sound2mimic()
-                duration = currgame.get_duration(mim_filename)
-                rep_mim = currgame.record_user(duration)
-                currgame.play_rec(rep_mim)
-                print("\nNot bad, {}!\n".format(currgame.username))
-                time = datetime.datetime.now()
-                time_str = "{}".format(str(time.year)+'_'+str(time.day)+'_'+str(time.hour)+'_'+str(time.minute)+'__'+str(time.second))
-                usr_recfilename = directory_user+username+'_'+time_str+'.wav'
-                currgame.save_rec(usr_recfilename,rep_mim,fs=44100)
-                fingpr_mim = currgame.get_fingpr(mim_filename)
-                fingpr_usr = currgame.get_fingpr(usr_recfilename)
-                score = currgame.comp_fingpr(fingpr_mim,fingpr_usr)
-                print('\nAnd your score is:\n',score)
-            else:
-                print("Thanks for playing!")
-                currgame.close_game()
+            while currgame.points < 1000:
+                currgame.cont_game = currgame.start_game('listen to a sound')
+                if currgame.cont_game:
+                    print("Right after this plays, we will record your attempt at the sound. Get ready!")
+                    mim_filename = directory_mim+currgame.rand_sound2mimic()
+                    duration = currgame.get_duration(mim_filename)
+                    rep_mim = currgame.record_user(duration)
+                    currgame.play_rec(rep_mim)
+                    print("\nNot bad, {}!\n".format(currgame.username))
+                    time = datetime.datetime.now()
+                    time_str = "{}".format(str(time.year)+'_'+str(time.day)+'_'+str(time.hour)+'_'+str(time.minute)+'__'+str(time.second))
+                    usr_recfilename = directory_user+username+'_'+time_str+'.wav'
+                    currgame.save_rec(usr_recfilename,rep_mim,fs=44100)
+                    fingpr_mim = currgame.get_fingpr(mim_filename)
+                    fingpr_usr = currgame.get_fingpr(usr_recfilename)
+                    score = currgame.comp_fingpr(fingpr_mim,fingpr_usr)
+                    currgame.points += score
+                    print('\nYour score for that mimic: ',score)
+                    print('\nTotal points collected so far: ',score)
+                else:
+                    print("Thanks for playing!")
+                    currgame.close_game()
+            print("\nCongratulations!!! You're a MIMIC MASTER!!")
+            currgame.cont_game = False
+            currgame.close_game()
+            
