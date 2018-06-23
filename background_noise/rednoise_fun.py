99b0cd089a5111b6b7c7cd16cf130c1478060d30
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-06-23 16:04:35 +0200

    amateur noise reduction scripts.... don't work yet.

---
+++ background_noise/rednoise_fun.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 14 16:14:17 2018
+
+Pulled code from: https://github.com/zhr1201/Multi-channel-speech-extraction-using-DNN/blob/master/multichannel_cnn/audio_eval.py
+~ def stft()
+
+Goal: trying to mimic what Audacity does in noise reduction
+https://wiki.audacityteam.org/wiki/
+Using same FFT window size as Audacity: 2048
+Which leads to 1025 frequency bands
+
+Other research using fft to capture speech features have used 25ms windows w 10ms window shifts
+
+@author: airos
+"""
+
+
+import numpy as np
+from numpy.lib import stride_tricks
+import librosa
+from scipy import signal
+
+#2048 based off of Audacity
+def stft(sig, frameSize= 2048, overlapFac=0.60, window=np.hanning):
+    """ short time fourier transform of audio signal """
+    win = window(frameSize)
+    hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+    # zeros at beginning (thus center of 1st window should be for sample nr. 0)
+    # samples = np.append(np.zeros(np.floor(frameSize / 2.0)), sig)
+    samples = np.array(sig, dtype='float64')
+    # cols for windowing
+    cols = np.ceil((len(samples) - frameSize) / float(hopSize)) + 1
+    # zeros at end (thus samples can be fully covered by frames)
+    # samples = np.append(samples, np.zeros(frameSize))
+    frames = stride_tricks.as_strided(
+        samples,
+        shape=(int(cols), frameSize),
+        strides=(samples.strides[0] * hopSize, samples.strides[0])).copy()
+    frames *= win
+    return np.fft.rfft(frames)
+
+def wave2stft(wavefile):
+    y, sr = librosa.load(wavefile,sr=None)
+    f,t,stft = signal.stft(y,sr,nperseg = int(sr*0.025),nfft=2048)
+    stft = np.transpose(stft)
+    return stft, sr
+
+def stft2power(stft_matrix):
+    stft = stft_matrix.copy()
+    power = np.abs(stft)**2
+    return(power)
+    
+def stft2amp(stft_matrix):
+    stft = stft_matrix.copy()
+    amp = np.abs(stft)
+    return amp
+
+def get_mean_bandwidths(matrix_bandwidths):
+    bw = matrix_bandwidths.copy()
+    bw_mean = [np.mean(bw[:,bandwidth]) for bandwidth in range(bw.shape[1])]
+    return bw_mean
+                                                               
+#def red_noise(noise_powerspec_mean,speech_powerspec_row, ):
+    #npm = noise_powerspec_mean
+    #spr = speech_powerspec_row.copy()
+    #for i in range(len(spr)):
+        #if spr[i] <= npm[i]:
+            #spr[i] = 1e-6
+        #else:
+            #diff = spr[i] - npm[i]
+            #spr[i] = diff
+    #return spr
+
+def rednoise_short(noise_powerspec_mean, speech_powerspec_row,speech_stft_row, col_start,col_len):
+    npm = noise_powerspec_mean[col_start:col_start+col_len]
+    spr = speech_powerspec_row[col_start:col_start+col_len]
+    stft_r = speech_stft_row[col_start:col_start+col_len].copy()
+    for i in range(len(spr)):
+        print(stft_r[i])
+        if spr[i] <= npm[i]:
+            stft_r[i] = 1e-3
+        else:
+            print(spr[i])
+            print(npm[i])
+            mag = npm[i]/float(spr[i])
+            print(mag)
+            stft_r[i] *= mag
+            print(stft_r[i])
+    return stft_r
+
+def rednoise(noise_powerspec_mean, speech_powerspec_row,speech_stft_row):
+    npm = noise_powerspec_mean
+    spr = speech_powerspec_row
+    stft_r = speech_stft_row.copy()
+    for i in range(len(spr)):
+        if spr[i] <= npm[i]:
+            stft_r[i] = 1e-3
+        else:
+            mag = npm[i]/float(spr[i])
+            stft_r[i] *= mag
+    return stft_r
+#
+#def red_noise(noise_powerspec_mean, speech_powerspec_row,speech_stft_row):
+#    npm = noise_powerspec_mean
+#    spr = speech_powerspec_row
+#    stft_r = speech_stft_row.copy()
+#    for i in range(len(spr)):
+#        if spr[i] <= npm[i]:
+#            stft_r[i] = 1e-3
+#        else:
+#            mag = npm[i]/float(spr[i])
+#            stft_r[i] *= np.sqrt(mag)
+#    return stft_r
+#            
+    
+
+def stft2wave(stft,sr):
+    istft = np.transpose(stft.copy())
+    f,samples = signal.istft(istft,fs=sr,nperseg=int(sr*0.025),nfft=2048)
+    return samples
+
+def savewave(filename,samples,sr):
+    librosa.output.write_wav(filename,samples,sr)
+    print("File has been saved")
+
+                                                        
+def power2stft(power_spec):
+    stft = np.sqrt(power_spec)
+    return stft
+
+def get_lengthwave(samples,sr):
+    len_sec = len(samples)/float(sr)
+    return len_sec
+
---
+++ background_noise/rednoise_main.py
@@ -0,0 +1,40 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from rednoise_fun import wave2stft, stft2power, stft2amp, get_mean_bandwidths, rednoise, stft2wave, savewave
+
+if __name__ == '__main__':
+    noise_file = 'Aislyn_2018_20_12_10__3.wav'
+    speech_file = 'Aislyn_2018_20_12_54__27.wav'
+    
+    #subtract noise 
+    
+    #get stft and sampling rage
+    noise_stft, noise_sr = wave2stft(noise_file)
+    speech_stft, speech_sr = wave2stft(speech_file)
+    
+    #get power
+    noise_amp = stft2amp(noise_stft)
+    sp_amp = stft2amp(speech_stft)
+    #get mean power (for each bandwidth)
+    noise_amp_mean = get_mean_bandwidths(noise_amp)
+    
+    #subtract noise from power and stft
+#    start_col = 215
+#    col_len = 30
+#    stft_red_100 = rednoise_short(noise_pw_mean,sp_amp[100],speech_stft[100],start_col,col_len) 
+    stft_red = [rednoise(noise_amp_mean,sp_amp[row],speech_stft[row]) for row in range(speech_stft.shape[0])]
+    
+    #transform stft back to wave
+    speech_red = stft2wave(stft_red,speech_sr)
+    savewave("rednoise1.wav",speech_red,speech_sr)
+    
+
+    
+
+    
+    
+    
+
+    
